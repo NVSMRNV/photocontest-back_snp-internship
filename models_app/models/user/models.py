97baca0c42b0f43886138f14c81e2a04bd4a2560
Nikita Smirnov <nvsmrnv@gmail.com>
2024-09-05 11:33:28 +0300

    update user model and delete profile model

--- models_app/models/user/models.py
+++ models_app/models/user/models.py
@@ -1,25 +1,26 @@
-from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from models_app.models.base.models import BaseModel
 
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    is_social_auth = models.BooleanField(default=False)
+class User(AbstractUser, BaseModel):
+    username = models.CharField('username', max_length=128, unique=True)
+    bio = models.CharField(max_length=512, blank=True, default='')
+    email = models.EmailField('email', blank=True)
+    avatar = models.ImageField(upload_to='users/', blank=True, null=True) #TODO Указать путь в upload_to
+    date_joined = None
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
     class Meta:
         db_table = 'users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self) -> str:
         return self.email
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    avatar = models.ImageField(upload_to='users/', blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
