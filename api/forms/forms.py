e27b862df171d252d3f9d930939457e60f26967f
Nikita Smirnov <nvsmrnv@gmail.com>
2024-09-11 19:58:16 +0300

    add form for user register

---
+++ api/forms/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from models_app.models.user.models import User
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
