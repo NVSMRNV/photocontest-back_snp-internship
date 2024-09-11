d47ec0fac792f2119ae266e5bebce0fd8cc860a2
Nikita Smirnov <nvsmrnv@gmail.com>
2024-09-11 20:06:04 +0300

    delete unnecessary files

--- api/serializers/user/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from models_app.models.user.models import User
-
-
-class UserSerializer(serializers.ModelSerializers):
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'username',)
-        
