c58af5bc5599c81476168bea20373712f3023f17
Nikita Smirnov <nvsmrnv@gmail.com>
2024-09-16 19:52:04 +0300

    add login and logut views

--- api/views/users/views.py
+++ api/views/users/views.py
@@ -1,9 +1,16 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
-from api.forms.forms import RegisterUserForm
+from api.forms.forms import LoginUserForm, RegisterUserForm
 
 
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'users/index.html')
+    
+    
 class RegisterUserView(CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
@@ -11,7 +18,9 @@ class RegisterUserView(CreateView):
 
 
 class LoginUserView(LoginView):
-    pass
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    redirect_authenticated_user = ''
 
 
 class LogoutUserView(LogoutView):
