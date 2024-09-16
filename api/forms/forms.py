5a3ef31c4db76d93375dd36f8bf48cae1f5873f1
Nikita Smirnov <nvsmrnv@gmail.com>
2024-09-16 19:49:48 +0300

    add login form

--- api/forms/forms.py
+++ api/forms/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from models_app.models.user.models import User
 
 
@@ -28,3 +28,20 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
