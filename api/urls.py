26a72796e0138526669d72ab9c265ca0b010913d
Nikita Smirnov <nvsmrnv@gmail.com>
2024-11-07 14:56:28 +0300

    conf url settings

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from api.views.users.views import LoginUserAPIView, RegisterUserAPIView, TestAPIView
+from api.views.photo.views import CreatePhotoAPIView, ListPhotoAPIView, RetrievePhotoAPIView
+from api.views.users.views import LoginUserAPIView, RegisterUserAPIView
 
 urlpatterns = [
+    path('photos/', ListPhotoAPIView.as_view(), name='list-photos'),
+    path('create/', CreatePhotoAPIView.as_view(), name='create-photo'),
+    path('retrieve/<int:pk>/', RetrievePhotoAPIView.as_view(), name='retrieve-photo'),
     path('register/', RegisterUserAPIView.as_view(), name='register' ),
     path('login/', LoginUserAPIView.as_view(), name='login'),
 ]
