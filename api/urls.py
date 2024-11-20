36cf354ad90406954175c1d891a3b119e559d853
Nikita Smirnov <nvsmrnv@gmail.com>
2024-11-20 21:31:19 +0300

    update api urls

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,30 @@
-from django.urls import path
+from django.urls import include, path
 
-from api.views.photo.views import CreatePhotoAPIView, ListPhotoAPIView, RetrievePhotoAPIView
 from api.views.users.views import LoginUserAPIView, RegisterUserAPIView
+from api.views.photo.views import CreatePhotoAPIView, ListPhotoAPIView, RetrievePhotoAPIView
+
+from api.docs.basics.schema import schema_view
+
+
+users_api_urlpatterns = [
+   path('login/', LoginUserAPIView.as_view(), name='users-login'),
+   path('registration/', RegisterUserAPIView.as_view(), name='users-registration' ),
+]
+
+photos_api_urlpatterns = [
+   path('list/', ListPhotoAPIView.as_view(), name='photos-list'),
+   path('creation/', CreatePhotoAPIView.as_view(), name='photos-creation'),
+   path('retrieval/<int:pk>/', RetrievePhotoAPIView.as_view(), name='photos-retrieval'),
+]
+
+docs_api_urlpatterns = [
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
 
 urlpatterns = [
-    path('photos/', ListPhotoAPIView.as_view(), name='list-photos'),
-    path('create/', CreatePhotoAPIView.as_view(), name='create-photo'),
-    path('retrieve/<int:pk>/', RetrievePhotoAPIView.as_view(), name='retrieve-photo'),
-    path('register/', RegisterUserAPIView.as_view(), name='register' ),
-    path('login/', LoginUserAPIView.as_view(), name='login'),
+   path('v1/users/', include(users_api_urlpatterns)),
+   path('v1/photos/', include(photos_api_urlpatterns)),
+   path('v1/docs/', include(docs_api_urlpatterns)),
 ]
