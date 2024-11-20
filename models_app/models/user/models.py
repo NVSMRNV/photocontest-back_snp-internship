65aa6803b8b2b4a38c3462a30ed83ae7dc632b75
Nikita Smirnov <nvsmrnv@gmail.com>
2024-11-20 21:46:01 +0300

    update user model

--- models_app/models/user/models.py
+++ models_app/models/user/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
+
 from models_app.models.base.models import BaseModel
+
+from utils.file_uploader import uploaded_file_path
 
 
 class User(AbstractUser, BaseModel):
     username = models.CharField('username', max_length=128, unique=True)
     bio = models.CharField(max_length=512, blank=True, default='')
     email = models.EmailField('email', blank=True)
-    avatar = models.ImageField(upload_to='users/', blank=True, null=True) #TODO Указать путь в upload_to и добивать дефолт
+    avatar = models.ImageField(upload_to=uploaded_file_path, blank=True, default='defaultuser.png')
+
     date_joined = None
+    first_name = None
+    last_name = None
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
